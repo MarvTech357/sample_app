42f64552261823617b22c81f91c9bfbde8589776
MarvTech357 <wishnwonder@gmail.com>
2014-05-14 20:40:20 -0700

    rspec revisions and help file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_content ('Sample App')
+     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/home'
+     page.should have_content ('Sample App')
+  end
+end
+
+ describe "Help page" do
+   it "should have the content 'Sample App'" do
+     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/help'
+     page.should have_content ('Sample App')
     end
   end
 end
